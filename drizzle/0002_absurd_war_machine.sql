15aefc6d00c94be244f2fd7b832cbdbbb5df4c78
Dutrak <fernandodutra100701@gmail.com>
2024-08-04 15:15:18 -0300

    feat:add users and resturants schemas

---
+++ drizzle/0002_absurd_war_machine.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "restaurant" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DROP TABLE "restaurants";
