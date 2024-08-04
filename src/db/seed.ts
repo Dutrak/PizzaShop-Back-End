3f524901ed6fa9988db26ded3a1a325ed14e8e7c
Dutrak <fernandodutra100701@gmail.com>
2024-08-04 16:26:15 -0300

    refactor:update all files with eslint rules

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,3 +1,5 @@
+/* eslint-disable drizzle/enforce-delete-with-where */
+
 import { faker } from '@faker-js/faker'
 import { users, restaurants } from './schema'
 import { db } from './connection'
@@ -10,7 +12,7 @@ import chalk from 'chalk'
 await db.delete(users)
 await db.delete(restaurants)
 
-console.log(chalk.yellow("🗸 Database Reset! "))
+console.log(chalk.yellow('🗸 Database Reset! '))
 
 /**
  * Create Costumers
@@ -20,32 +22,33 @@ await db.insert(users).values([
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: 'customer'
+    role: 'customer',
   },
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: 'customer'
-  }
+    role: 'customer',
+  },
 ])
 
-console.log(chalk.yellow("🗸 Created Costumers! "))
-
+console.log(chalk.yellow('🗸 Created Costumers! '))
 
 /**
  * Create Manager
  */
 
-const [ manager ] = await db.insert(users).values([
-  {
-    name: faker.person.fullName(),
-    email: 'admin@admin.com',
-    role: 'manager'
-  },
-]).returning({ id: users.id })
+const [manager] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: 'admin@admin.com',
+      role: 'manager',
+    },
+  ])
+  .returning({ id: users.id })
 
-console.log(chalk.yellow("🗸 Created Manager! "))
-
+console.log(chalk.yellow('🗸 Created Manager! '))
 
 /**
  * Create Restaurant
@@ -55,11 +58,11 @@ await db.insert(restaurants).values([
   {
     name: faker.company.name(),
     description: faker.lorem.paragraph(),
-    managerId: manager.id
+    managerId: manager.id,
   },
 ])
 
-console.log(chalk.yellow("🗸 Created Managers! "))
-console.log(chalk.greenBright("Database Seeded Sucessfully!"))
+console.log(chalk.yellow('🗸 Created Managers! '))
+console.log(chalk.greenBright('Database Seeded Sucessfully!'))
 
 process.exit()
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,8 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia()
-  .get('/', () => {
-    return 'Hello Elysia'
-  })
+const app = new Elysia().get('/', () => {
+  return 'Hello Elysia'
+})
 
 app.listen(3333, () => {
   console.log('Http Server Running')
