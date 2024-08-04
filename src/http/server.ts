c22627e84bb58c88f8c2e03fc591312893a8294d
Dutrak <fernandodutra100701@gmail.com>
2024-08-04 18:21:35 -0300

    feat:add elysia jwt and cookies

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,40 +1,23 @@
 import { Elysia, t } from 'elysia'
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({
-        id: users.id,
-      })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    })
-
-    set.status = 201
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      phone: t.String(),
-      email: t.String({ format: 'email' }),
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
     }),
-  },
-)
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('Http Server Running')
