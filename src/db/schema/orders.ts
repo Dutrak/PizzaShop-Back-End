1eb44d41197dd00e7a532571553fffb7fd3b0dac
Dutrak <fernandodutra100701@gmail.com>
2024-08-04 22:37:32 -0300

    feat:add drizzle relations

--- src/db/schema/orders.ts
+++ src/db/schema/orders.ts
@@ -2,6 +2,8 @@ import { createId } from '@paralleldrive/cuid2'
 import { text, timestamp, pgTable, pgEnum, integer } from 'drizzle-orm/pg-core'
 import { users } from './users'
 import { restaurants } from './restuarants'
+import { relations } from 'drizzle-orm'
+import { orderItems } from './order-items'
 
 export const orderStatusEnum = pgEnum('order_status', [
   'pending',
@@ -26,4 +28,20 @@ export const orders = pgTable('orders', {
   status: orderStatusEnum('status').default('pending'),
   totalInCents: integer('total_in_cents').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
+})
+
+export const ordersRelations = relations(orders, ({ one, many }) => {
+  return {
+    customer: one(users, {
+      fields: [orders.costumerId],
+      references: [users.id],
+      relationName: 'order_costumer',
+    }),
+    restaurant: one(restaurants, {
+      fields: [orders.restaurantId],
+      references: [restaurants.id],
+      relationName: 'order_restaurant',
+    }),
+    ordersItems: many(orderItems),
+  }
 })
--- src/db/schema/products.ts
+++ src/db/schema/products.ts
@@ -1,6 +1,8 @@
 import { createId } from '@paralleldrive/cuid2'
 import { text, timestamp, pgTable, integer } from 'drizzle-orm/pg-core'
 import { restaurants } from './restuarants'
+import { relations } from 'drizzle-orm'
+import { orderItems } from './order-items'
 
 export const products = pgTable('products', {
   id: text('id')
@@ -16,4 +18,15 @@ export const products = pgTable('products', {
     }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const productsRelations = relations(products, ({ one, many }) => {
+  return {
+    customer: one(restaurants, {
+      fields: [products.restaurantId],
+      references: [restaurants.id],
+      relationName: 'product_restaurant',
+    }),
+    ordersItems: many(orderItems),
+  }
 })
