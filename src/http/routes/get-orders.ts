00716617d685cfc863698b0a1fc9f6ae5ad06026
Dutrak <fernandodutra100701@gmail.com>
2024-08-05 00:20:30 -0300

    feat:add get all orders route

---
+++ src/http/routes/get-orders.ts
@@ -0,0 +1,75 @@
+import Elysia, { t } from 'elysia'
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { createSelectSchema } from 'drizzle-typebox'
+import { orders, users } from '../../db/schema'
+import { and, count, desc, eq, ilike, sql } from 'drizzle-orm'
+
+export const getOrders = new Elysia().use(auth).get(
+  '/orders',
+  async ({ getCurrentUser, query }) => {
+    const { restaurantId } = await getCurrentUser()
+    const { pageIndex, costumerName, orderId, status } = query
+
+    if (!restaurantId) throw new UnauthorizedError()
+
+    const baseQuery = db
+      .select({
+        orderId: orders.id,
+        createdAt: orders.createdAt,
+        status: orders.status,
+        total: orders.totalInCents,
+        costumerName: users.name,
+      })
+      .from(orders)
+      .innerJoin(users, eq(users.id, orders.costumerId))
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          orderId ? ilike(orders.id, `%${orderId}%`) : undefined,
+          status ? eq(orders.status, status) : undefined,
+          costumerName ? ilike(users.name, `%${costumerName}%`) : undefined,
+        ),
+      )
+
+    const [[{ count: amountOfOrders }], allOrders] = await Promise.all([
+      db.select({ count: count() }).from(baseQuery.as('baseQuery')),
+      db
+        .select()
+        .from(baseQuery.as('baseQuery'))
+        .offset(pageIndex * 10)
+        .limit(10)
+        .orderBy((fields) => {
+          return [
+            sql`CASE ${fields.status}
+              WHEN 'pending' THEN 1
+              WHEN 'processing' THEN 2
+              WHEN 'delivering' THEN 3
+              WHEN 'delivered' THEN 4
+              WHEN 'canceled' THEN 99
+            END
+            `,
+            desc(fields.createdAt),
+          ]
+        }),
+    ])
+
+    return {
+      orders: allOrders,
+      meta: {
+        pageIndex,
+        perPage: 10,
+        totalCount: amountOfOrders,
+      },
+    }
+  },
+  {
+    query: t.Object({
+      costumerName: t.Optional(t.String()),
+      orderId: t.Optional(t.String()),
+      status: t.Optional(createSelectSchema(orders).properties.status),
+      pageIndex: t.Numeric({ minimum: 0 }),
+    }),
+  },
+)
